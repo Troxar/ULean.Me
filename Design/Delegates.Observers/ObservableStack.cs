3c0623ef948c5a9ecc6720bee8e5e58f028940c1
Alexander Katkov <afargoth@gmail.com>
2023-06-13 19:39:50 +0700

    Get rid of redundant Observer class in Delegates.Observers

--- Design/Delegates.Observers/ObservableStack.cs
+++ Design/Delegates.Observers/ObservableStack.cs
@@ -5,56 +5,43 @@ using System.Text;
 namespace Delegates.Observers
 {
     public class StackOperationsLogger
-	{
-        private readonly IObserver observer = new Observer();
-        
+    {
+        private readonly StringBuilder _sb = new StringBuilder();
+
         public void SubscribeOn<T>(ObservableStack<T> stack)
-		{
-			stack.Notify += observer.HandleEvent;
-		}
+        {
+            stack.Notify += HandleEvent;
+        }
+
+        public void HandleEvent(object eventData)
+        {
+            _sb.Append(eventData);
+        }
 
         public string GetLog()
-		{
-			return observer.Log.ToString();
-		}
-	}
-
-	public interface IObserver
-	{
-		string Log { get; }
-        void HandleEvent(object eventData);
-	}
-
-	public class Observer : IObserver
-	{
-		private readonly StringBuilder _sb = new StringBuilder();
-        public string Log => _sb.ToString();
-
-		public void HandleEvent(object eventData)
-		{
-			_sb.Append(eventData);
-		}
-	}
+        {
+            return _sb.ToString();
+        }
+    }
 
     public class ObservableStack<T>
     {
-		public event Action<StackEventData<T>> Notify;
+        private readonly List<T> data = new List<T>();
+        public event Action<StackEventData<T>> Notify;
 
-		private readonly List<T> data = new List<T>();
+        public void Push(T obj)
+        {
+            data.Add(obj);
+            Notify?.Invoke(new StackEventData<T> { IsPushed = true, Value = obj });
+        }
 
-		public void Push(T obj)
-		{
-			data.Add(obj);
-			Notify?.Invoke(new StackEventData<T> { IsPushed = true, Value = obj });
-		}
-
-		public T Pop()
-		{
-			if (data.Count == 0)
-				throw new InvalidOperationException();
-			var result = data[data.Count - 1];
-			Notify?.Invoke(new StackEventData<T> { IsPushed = false, Value = result });
-			return result;
-		}
-	}
+        public T Pop()
+        {
+            if (data.Count == 0)
+                throw new InvalidOperationException();
+            var result = data[data.Count - 1];
+            Notify?.Invoke(new StackEventData<T> { IsPushed = false, Value = result });
+            return result;
+        }
+    }
 }
