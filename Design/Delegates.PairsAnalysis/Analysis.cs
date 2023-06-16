ac5c54ede4ffed3b54714dde5bbea8257b9141e4
Alexander Katkov <afargoth@gmail.com>
2023-06-16 14:16:44 +0700

    Add solution for Delegates.PairsAnalysis

--- Design/Delegates.PairsAnalysis/Analysis.cs
+++ Design/Delegates.PairsAnalysis/Analysis.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Delegates.PairsAnalysis
 {
@@ -10,12 +8,74 @@ namespace Delegates.PairsAnalysis
     {
         public static int FindMaxPeriodIndex(params DateTime[] data)
         {
-            return new MaxPauseFinder().Analyze(data);
+            return data
+                .Pairs()
+                .ProcessItems(pair => (pair.Item2 - pair.Item1).TotalSeconds)
+                .MaxIndex();
         }
 
         public static double FindAverageRelativeDifference(params double[] data)
         {
-            return new AverageDifferenceFinder().Analyze(data);
+            return data
+                .Pairs()
+                .ProcessItems(pair => (pair.Item2 - pair.Item1) / pair.Item1)
+                .AverageDifference();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<Tuple<T, T>> Pairs<T>(this IEnumerable<T> source)
+        {
+            T prev = default;
+            int counter = 0;
+
+            foreach (T value in source)
+            {
+                if (counter > 0)
+                    yield return Tuple.Create(prev, value);
+                prev = value;
+                counter++;
+            }
+
+            if (counter < 2)
+                throw new InvalidOperationException("Not enough items in collection");
+        }
+
+        public static int MaxIndex<T>(this IEnumerable<T> source)
+            where T : IComparable
+        {
+            T max = default;
+            int bestIndex = 0;
+            int counter = 0;
+
+            foreach (T value in source)
+            {
+                if (counter == 0 || value.CompareTo(max) > 0)
+                {
+                    max = value;
+                    bestIndex = counter;
+                }
+                counter++;
+            }
+
+            if (counter == 0)
+                throw new InvalidOperationException("Collection is empty");
+
+            return bestIndex;
+        }
+
+        public static IEnumerable<TResult> ProcessItems<TSource, TResult>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TResult> processor)
+        {
+            foreach (TSource value in source)
+                yield return processor(value);
+        }
+
+        public static double AverageDifference(this IEnumerable<double> source)
+        {
+            return source.Average();
         }
     }
 }
