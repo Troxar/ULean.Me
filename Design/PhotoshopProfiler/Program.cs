cd3a5314eb12d9fb2522116cc7f80e2a94e6a1f4
Alexander Katkov <afargoth@gmail.com>
2023-06-18 15:11:24 +0700

    Add Profiler project for performance testing in Photoshop

---
+++ Design/PhotoshopProfiler/Program.cs
@@ -0,0 +1,32 @@
+﻿using MyPhotoshop;
+using System.Diagnostics;
+
+namespace Profiler
+{
+    internal class Program
+    {
+        private static void Test(Action<double[], LighteningParameters> action, int count)
+        {
+            var args = new double[] { 0 };
+            var parameters = new LighteningParameters();
+            action(args, parameters);
+
+            var sw = new Stopwatch();
+            sw.Start();
+
+            for (int i = 0; i < count; i++)
+                action(args, parameters);
+
+            sw.Stop();
+
+            Console.WriteLine(sw.ElapsedMilliseconds * 1000d / count);
+        }
+
+        public static void Main(string[] args)
+        {
+            int count = 100000;
+            Test((values, parameters) => parameters.Parse(values), count);
+            Test((values, parameters) => parameters.Coefficient = values[0], count);
+        }
+    }
+}
