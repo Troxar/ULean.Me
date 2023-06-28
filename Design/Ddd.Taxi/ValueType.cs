b461c82f01a07d329b98f88160b5b7f129d5c9db
Alexander Katkov <afargoth@gmail.com>
2023-06-28 21:36:47 +0700

    Use Expressions for optimization in Ddd.Taxi

--- Design/Ddd.Taxi/ValueType.cs
+++ Design/Ddd.Taxi/ValueType.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Reflection;
 
 namespace Ddd.Infrastructure
@@ -8,35 +8,203 @@ namespace Ddd.Infrastructure
     /// <summary>
     /// Базовый класс для всех Value типов.
     /// </summary>
-    public class ValueType<T>
+    public abstract class ValueType<T> where T : class
     {
-        private static string _typeName;
-        private static IEnumerable<PropertyInfo> _publicProperties;
+        private static Func<T, string> _toStringGenerator;
+        private static Func<T, T, bool> _equalsDecider;
+        private static Func<T, int> _hashCodeGenerator;
+
+        private int? _hashCode;
 
         static ValueType()
         {
-            Type type = typeof(T);
             var bindingFlags = BindingFlags.Instance | BindingFlags.Public;
-            _publicProperties = type
+            var publicProperties = typeof(T)
                 .GetProperties(bindingFlags)
-                .OrderBy(prop => prop.Name);
-            _typeName = type.Name;
+                .OrderBy(prop => prop.Name)
+                .ToArray();
+
+            _toStringGenerator = CreateToStringGenerator(publicProperties);
+            _equalsDecider = CreateEqualsDecider(publicProperties);
+            _hashCodeGenerator = CreateHashCodeGenerator(publicProperties);
         }
+
+        #region ToStringGenerator
+
+        private static Func<T, string> CreateToStringGenerator(PropertyInfo[] properties)
+        {
+            // (instance) => "TypeName(Property1: Value1; Property2: Value2; ...)"
+
+            Type type = typeof(T);
+            var parameterExpr = Expression.Parameter(type, "instance");
+
+            var joinArgs = new Expression[2];
+            joinArgs[0] = Expression.Constant("; ");
+            joinArgs[1] = Expression.NewArrayInit(typeof(string),
+                GetArrayExpressionForStringGenerator(properties, parameterExpr));
+
+            var joinExpr = Expression.Call(
+                typeof(string).GetMethod("Join", new Type[] { typeof(string), typeof(string[]) }),
+                joinArgs);
+
+            return Expression.Lambda<Func<T, string>>(
+                 GetConcatExpressionForToStringGenerator(type, joinExpr), parameterExpr)
+                .Compile();
+        }
+
+        private static MethodCallExpression GetConcatExpressionForToStringGenerator(
+            Type type, MethodCallExpression joinExpr)
+        {
+            var concatArgs = new Expression[4];
+            concatArgs[0] = Expression.Constant(type.Name);
+            concatArgs[1] = Expression.Constant("(");
+            concatArgs[2] = joinExpr;
+            concatArgs[3] = Expression.Constant(")");
+
+            return Expression.Call(
+                typeof(string).GetMethod("Concat",
+                    new[] { typeof(string), typeof(string), typeof(string), typeof(string) }),
+                concatArgs);
+        }
+
+        private static Expression[] GetArrayExpressionForStringGenerator(
+            PropertyInfo[] properties, ParameterExpression parameterExpr)
+        {
+            var arrayArgs = new Expression[properties.Length];
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var propertyExpr = Expression.Property(parameterExpr, properties[i]);
+
+                var concatArgs = new Expression[2];
+                concatArgs[0] = Expression.Constant($"{properties[i].Name}: ");
+                concatArgs[1] = properties[i].PropertyType.IsValueType
+                    ? Expression.Call(propertyExpr, "ToString", null)
+                    : (Expression)Expression.Condition(
+                        Expression.Equal(propertyExpr, Expression.Constant(null)),
+                        Expression.Constant(string.Empty),
+                        Expression.Call(propertyExpr, "ToString", null));
+
+                arrayArgs[i] = Expression.Call(
+                    typeof(string).GetMethod("Concat", new[] { typeof(string), typeof(string) }),
+                    concatArgs);
+            }
+
+            return arrayArgs;
+        }
+
+        #endregion
+
+        #region EqualsDecider
+
+        private static Func<T, T, bool> CreateEqualsDecider(PropertyInfo[] properties)
+        {
+            // (left, right) => true && (Equals(left.Property1, right.Property1)
+            //      && (Equals(left.Property2, right.Property2)
+            //          && ... ))
+
+            Type type = typeof(T);
+            var parameterLeftExpr = Expression.Parameter(type, "left");
+            var parameterRightExpr = Expression.Parameter(type, "right");
+
+            if (properties.Length == 0)
+                return Expression.Lambda<Func<T, T, bool>>(Expression.Constant(true),
+                    new[] { parameterLeftExpr, parameterRightExpr })
+                    .Compile();
+
+            var andExpr = GetAndExpressionForEqualsDecider(properties,
+                parameterLeftExpr, parameterRightExpr);
+
+            return Expression.Lambda<Func<T, T, bool>>(andExpr,
+                new[] { parameterLeftExpr, parameterRightExpr })
+                .Compile();
+        }
+
+        private static BinaryExpression GetAndExpressionForEqualsDecider(
+            PropertyInfo[] properties, ParameterExpression parameterLeftExpr,
+            ParameterExpression parameterRightExpr)
+        {
+            BinaryExpression andExpr = null;
+            var objectEquals = typeof(object).GetMethod("Equals", new[] { typeof(object), typeof(object) });
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var propertyLeftExpr = Expression.Property(parameterLeftExpr, properties[i]);
+                var propertyRightExpr = Expression.Property(parameterRightExpr, properties[i]);
+                var equalsExpr = properties[i].PropertyType.IsValueType
+                    ? Expression.Call(propertyLeftExpr,
+                        properties[i].PropertyType.GetMethod("Equals", new[] { properties[i].PropertyType }),
+                        propertyRightExpr)
+                    : Expression.Call(
+                        objectEquals,
+                        new[] { propertyLeftExpr, propertyRightExpr });
+                andExpr = i == 0
+                    ? Expression.AndAlso(Expression.Constant(true), equalsExpr)
+                    : Expression.AndAlso(andExpr, equalsExpr);
+            }
+
+            return andExpr;
+        }
+
+        #endregion
+
+        #region HashCodeGenerator
+
+        private static Func<T, int> CreateHashCodeGenerator(PropertyInfo[] properties)
+        {
+            //int seed = 397;
+            //_hashCode = seed;
+            //foreach (var prop in _publicProperties)
+            //    _hashCode = unchecked((_hashCode ^ prop.GetValue(this).GetHashCode()) * seed);
+
+            var parameterExpr = Expression.Parameter(typeof(T), "instance");
+            var seedExpr = Expression.Constant(397);
+
+            if (properties.Length == 0)
+                return Expression.Lambda<Func<T, int>>(seedExpr,
+                    new[] { parameterExpr })
+                    .Compile();
+
+            var hashExpr = GetHashExpressionForHashCodeGenerator(
+                properties, parameterExpr, seedExpr);
+
+            return Expression.Lambda<Func<T, int>>(hashExpr,
+                new[] { parameterExpr })
+                .Compile();
+        }
+
+        private static Expression GetHashExpressionForHashCodeGenerator(
+            PropertyInfo[] properties, ParameterExpression parameterExpr, ConstantExpression seedExpr)
+        {
+            Expression hashExpr = seedExpr;
+
+            for (int i = 0; i < properties.Length; i++)
+            {
+                var propertyExpr = Expression.Property(parameterExpr, properties[i]);
+
+                var callExpr = properties[i].PropertyType.IsValueType
+                    ? Expression.Call(propertyExpr, "GetHashCode", null)
+                    : (Expression)Expression.Condition(
+                        Expression.Equal(propertyExpr, Expression.Constant(null)),
+                        Expression.Constant(0),
+                        Expression.Call(propertyExpr, "GetHashCode", null));
+
+                var xorExpr = Expression.ExclusiveOr(hashExpr, callExpr);
+                hashExpr = Expression.Multiply(xorExpr, seedExpr);
+            }
+
+            return hashExpr;
+        }
+
+        #endregion
 
         protected bool Equals(ValueType<T> other)
         {
-            return _publicProperties
-                .All(prop =>
-                {
-                    var value1 = prop.GetValue(this);
-                    var value2 = prop.GetValue(other);
-                    return value1?.Equals(value2) ?? value1 == value2;
-                });
+            return _equalsDecider(this as T, other as T);
         }
 
         public bool Equals(T other)
         {
-            return (other as ValueType<T>)?.Equals(this) ?? false;
+            return Equals(this, other);
         }
 
         public override bool Equals(object obj)
@@ -49,20 +217,17 @@ namespace Ddd.Infrastructure
 
         public override int GetHashCode()
         {
-            int seed = 397;
-            int hash = 0;
-            
-            foreach (var prop in _publicProperties)
-                hash = unchecked((hash ^ prop.GetValue(this).GetHashCode()) * seed);
+            if (_hashCode.HasValue)
+                return _hashCode.Value;
 
-            return hash;
+            _hashCode = _hashCodeGenerator(this as T);
+
+            return _hashCode.Value;
         }
 
         public override string ToString()
         {
-            var props = string.Join("; ", _publicProperties
-                .Select(prop => $"{prop.Name}: {prop.GetValue(this)}"));
-            return $"{_typeName}({props})";
+            return _toStringGenerator(this as T);
         }
     }
 }
