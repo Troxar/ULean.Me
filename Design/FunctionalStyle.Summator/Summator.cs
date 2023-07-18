088e0b9a1594f84243b5082f8063aee509bfc2df
Alexander Katkov <afargoth@gmail.com>
2023-07-18 10:22:22 +0700

    Add solution for FunctionalStyle.Summator

--- Design/FunctionalStyle.Summator/Summator.cs
+++ Design/FunctionalStyle.Summator/Summator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
@@ -19,22 +20,58 @@ namespace FP
 
         public void Process()
         {
-            using (var input = openDatasource())
-            using (var writer = new StreamWriter(outputFilename))
+            var res = SumRecords(openDatasource(), formatter)
+                .AfterEvery(100, c => Console.WriteLine("processed {0} items", c));
+            File.WriteAllLines(outputFilename, res);
+        }
+
+        public static IEnumerable<string> SumRecords(DataSource dataSource, ISumFormatter formatter)
+        {
+            return dataSource
+                .ReadIntRecords(16)
+                .Select(args => formatter.Format(args, args.Sum()));
+        }
+    }
+
+    public static class DataSourceExtensions
+    {
+        public static IEnumerable<string[]> ReadRecords(this DataSource data)
+        {
+            return Enumeration.RepeatUntilNull(data.NextRecord);
+        }
+
+        public static IEnumerable<int[]> ReadIntRecords(this DataSource data, int radix)
+        {
+            return data.ReadRecords()
+                .Select(record => record.Select(f => Convert.ToInt32(f, radix))
+                .ToArray());
+        }
+    }
+
+    public static class Enumeration
+    {
+        public static IEnumerable<T> RepeatUntilNull<T>(Func<T> get)
+        {
+            return Repeat(get).TakeWhile(i => i != null);
+        }
+
+        public static IEnumerable<T> Repeat<T>(Func<T> get)
+        {
+            while (true) yield return get();
+            // ReSharper disable once IteratorNeverReturns
+        }
+
+        public static IEnumerable<T> AfterEvery<T>(
+            this IEnumerable<T> items,
+            int period,
+            Action<int> afterNth)
+        {
+            var n = 0;
+            foreach (var item in items)
             {
-                var c = 0;
-                while (true)
-                {
-                    string[] record = input.NextRecord();
-                    if (record == null) break;
-                    c++;
-                    var nums = record.Select(part => Convert.ToInt32(part, 16)).ToArray();
-                    var sum = nums.Sum();
-                    var text = formatter.Format(nums, sum);
-                    writer.WriteLine(text);
-                    if (c % 100 == 0)
-                        Console.WriteLine("processed {0} items", c);
-                }
+                n++;
+                yield return item;
+                if (n % period == 0) afterNth(n);
             }
         }
     }
