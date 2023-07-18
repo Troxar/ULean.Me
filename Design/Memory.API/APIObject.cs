8f7921ee9781d018b027a992056da28c40830656
Alexander Katkov <afargoth@gmail.com>
2023-07-18 13:26:46 +0700

    Add solution for Memory.API

--- Design/Memory.API/APIObject.cs
+++ Design/Memory.API/APIObject.cs
@@ -1,10 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Memory.API
 {
-    
+    public class APIObject : IDisposable
+    {
+        private readonly int _id;
+        private bool _disposed;
+
+        public APIObject(int id)
+        {
+            _id = id;
+            MagicAPI.Allocate(id);
+        }
+
+        ~APIObject()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                MagicAPI.Free(_id);
+                _disposed = true;
+            }
+        }
+    }
 }
