c4c254296057bdbfdfd282caf99242bcf71e0d6f
Alexander Katkov <afargoth@gmail.com>
2023-07-18 09:47:19 +0700

    Add Design/FunctionalStyle.Summator

---
+++ Design/FunctionalStyle.Summator/Summator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace FP
+{
+    public class Summator
+    {
+        private readonly ISumFormatter formatter;
+        private readonly Func<DataSource> openDatasource;
+        private readonly string outputFilename;
+        
+        public Summator(Func<DataSource> openDatasource, ISumFormatter formatter, string outputFilename)
+        {
+            this.openDatasource = openDatasource;
+            this.formatter = formatter;
+            this.outputFilename = outputFilename;
+        }
+
+        public void Process()
+        {
+            using (var input = openDatasource())
+            using (var writer = new StreamWriter(outputFilename))
+            {
+                var c = 0;
+                while (true)
+                {
+                    string[] record = input.NextRecord();
+                    if (record == null) break;
+                    c++;
+                    var nums = record.Select(part => Convert.ToInt32(part, 16)).ToArray();
+                    var sum = nums.Sum();
+                    var text = formatter.Format(nums, sum);
+                    writer.WriteLine(text);
+                    if (c % 100 == 0)
+                        Console.WriteLine("processed {0} items", c);
+                }
+            }
+        }
+    }
+}
